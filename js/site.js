1d1f8af10a7862b3dbd9a6bc02e41eabeb839d8d
Sai Dinesh Tumu <saidinesh_tumu@outlook.com>
2017-07-10 03:11:27 +0530

    Changes to design

--- js/site.js
+++ js/site.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $.material.init();
     $('.toggler').on('click',function() {
         $(this).parent().children().toggle();
         $(this).parent().parent().find('.toggled_content').slideToggle(); 
+    });
+    $('.toggler_child').on('click',function() {
+        $(this).parent().children().toggle();
+        $(this).parent().parent().find('.toggled_content_child').slideToggle(); 
     });
 });
